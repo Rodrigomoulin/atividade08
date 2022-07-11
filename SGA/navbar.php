eeff37a26475a594a23c6ece3cd7000427c1dd76
Rodrigomoulin <rodrigomoulin021@gmail.com>
2022-07-10 23:39:08 -0300

    atividade09

--- SGA/navbar.php
+++ SGA/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Loja</a>
+    <a class="navbar-brand" href="#">SGA</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -11,11 +11,11 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Cliente
+            Aluno
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="formulario-cadastro-cliente.php">Cadastro</a></li>
-            <li><a class="dropdown-item" href="listagem_de_clientes.php">Listagem</a></li>
+            <li><a class="dropdown-item" href="formulario-cadastro-aluno.php">Cadastro</a></li>
+            <li><a class="dropdown-item" href="listagem-de-alunos.php">Listagem</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Busca</a></li>
           </ul>
@@ -24,11 +24,10 @@
           <a class="nav-link disabled"><?= $_SESSION['login']->email ?></a>
         </li>
       </ul>
-      <form id="formSearchName" class="d-flex" role="search" method="post" action="localizaCliente.php">
-        <input id="searchName" class="form-control me-2" name="nomeCliente" type="search" placeholder="Informe o nome" aria-label="Search"> 
-        
-
+      <form id="formSearchName" class="d-flex" role="search" method="post" action="localiza-aluno.php">
+        <input id="searchName" class="form-control me-2" name="nomeAluno" type="search" placeholder="Informe o nome" aria-label="Search">
+        <a class="btn btn-outline-danger" href="logout.php">Logout</a>
       </form>
     </div>
   </div>
-</nav> 
+</nav>
